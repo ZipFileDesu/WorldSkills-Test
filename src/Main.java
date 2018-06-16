4fabdca217459b04abc0246aa4816ad3f7cf01e0
ZipFileDesu <danila9794@gmail.com>
2018-06-16 22:16:00 +1000

    Добавлена документация

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * @author ZipFileDesu
  * This is main class, where I initialize my programm and open window "main menu"
+ * @author ZipFileDesu
  */
 public class Main extends Application {
 
     /**
-     * Here I open window "main menu" and set resolution 1024x768 and set window not resizable
+     * Here I open window "MainMenu.fxml" and set resolution 1024x768 and set window not resizable
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1024, 768));
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        primaryStage.setTitle("WSR 2018");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
-        primaryStage.setResizable(false);
+        primaryStage.hide();
+        //primaryStage.setResizable(false);
         DB_Handler db = new DB_Handler();
         db.getDBConnection();
     }
